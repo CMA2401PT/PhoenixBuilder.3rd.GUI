3d979600a79ce53d39041ba9a9295b9b8fc8ba31
daijianbang <1634268014@qq.com>
2022-02-25 09:56:41 +0800

    Create fyne_cross_compile.sh

---
+++ fyne_cross_compile.sh
@@ -0,0 +1,11 @@
+ICON="-icon unbundled_assets/Icon.png"
+VERSION="-app-version 0.0.3"
+APPID='-app-id "phoenixbuilder.third.gui"'
+APPBUILD='-app-build 145'
+ARGS="$ICON $VERSION $APPID $APPBUILD"
+
+fyne-cross linux -arch=amd64 $ARGS -name "FastBuilder_3rd_Gui"
+fyne-cross darwin -arch=amd64 $ARGS  -name "FastBuilder_3rd_Gui"
+fyne-cross windows -arch=amd64 $ARGS -name "FastBuilder_3rd_Gui.exe"
+fyne-cross android -arch=arm64 $ARGS -name "FastBuilder_3rd_Gui"
+fyne-cross ios $ARGS -name "FastBuilder-3rd-Gui"
